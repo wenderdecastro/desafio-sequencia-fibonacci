f42bc90233e18079f3bf3acaaba786296b9545ca
Wender <wendermaiac@gmail.com>
2023-04-09 19:11:22 -0300

    Desafio finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int numero1 = 1;
+int numero2 = 0;
+int numeroTotal = 0;
+while(numeroTotal < 500)
+{
+    numeroTotal = numero1 + numero2;
+    Console.WriteLine($"{numeroTotal}");
+    numero1 = numero2;
+    numero2 = numeroTotal;
+}
