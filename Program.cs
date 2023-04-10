40a8c2d88a1f2e8cf911411510994344b61b3d58
Wender <125275495+wenderdecastro@users.noreply.github.com>
2023-04-10 07:50:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ int numeroTotal = 0;
 while(numeroTotal < 500)
 {
     numeroTotal = numero1 + numero2;
-    Console.WriteLine($"{numeroTotal}");
+    Console.WriteLine(numeroTotal);
     numero1 = numero2;
     numero2 = numeroTotal;
 }
